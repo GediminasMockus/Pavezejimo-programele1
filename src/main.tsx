0d576fcc1d3bb7963166c4bae21b1567c98b94fc
GediminasMockus <84835174+GediminasMockus@users.noreply.github.com>
2026-09-13 12:18:46 +0300

    fix: prevent global translation observer from blocking app startup

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { useUiTranslation } from '@/lib/useUiTranslation';
-
-function TranslatedApp() {
-  useUiTranslation();
-  return <App />;
-}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TranslatedApp />
+    <App />
   </StrictMode>
 );
