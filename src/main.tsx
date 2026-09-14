4d772b0a8bfeb0d376be22610c01ac9befe76630
GediminasMockus <84835174+GediminasMockus@users.noreply.github.com>
2026-09-14 11:42:53 +0300

    stability: protect app with error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import { AppErrorBoundary } from './components/AppErrorBoundary';
 import { UiTranslationBridge } from './components/UiTranslationBridge';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <UiTranslationBridge />
-    <App />
+    <AppErrorBoundary>
+      <UiTranslationBridge />
+      <App />
+    </AppErrorBoundary>
   </StrictMode>
 );
